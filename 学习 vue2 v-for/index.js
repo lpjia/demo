f582043baf4c7f8e8e7047321b3a91d282b443ca
lpjia <420664957@qq.com>
2021-11-03 17:31:07 +0800

    v-for 上面直接操作数组

---
+++ 学习 vue2 v-for/index.js
@@ -0,0 +1,40 @@
+let vue = new Vue({
+  el: '#app',
+  data: {
+    sensorSum: [],
+    parentMessage: 'Parent',
+    myObject: {
+      title: 'How to do lists in Vue',
+      author: 'Jane Doe',
+      publishedAt: '2016-04-10'
+    }
+
+  },
+  computed: {
+    sensorSumSplit() {
+      return this.sensorSum.length > 5 ? 5 : this.sensorSum.length
+    },
+
+  },
+  created() {
+  },
+  mounted() {
+    this.init()
+    console.log(Object.keys(this.myObject))
+  },
+  methods: {
+    /**
+     * 
+     */
+    init() {
+      this.getList()
+    },
+    async getList() {
+      let response = await fetch('list.json');
+      if (!response.ok) throw new Error('response failed')
+      let res = await response.json();
+      // console.log('res: ', res)
+      this.sensorSum = res
+    },
+  }
+})
