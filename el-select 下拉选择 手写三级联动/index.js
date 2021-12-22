dede18f444a363c6150345e1a862a37d2b1af47a
lpjia <420664957@qq.com>
2021-12-22 23:15:43 +0800

    20211222

---
+++ el-select 下拉选择 手写三级联动/index.js
@@ -0,0 +1,91 @@
+import { RAP2_TUNNEL_ONLINE_MONITORING } from '../global/globalConfig.js'
+import { fetchGet } from '../util/fetchRequest.js'
+
+const vm = new Vue({
+  el: '#app',
+  data: {
+    // 搜索项
+    formSearchTerm: {
+      tunnel: '-1',
+      device: '',
+      channel: '',
+    },
+
+    allTunnelArr: [],
+    allDtsDeviceArr: [],
+    allDtsChannelArr: [],
+
+    canSelectDevice: false,
+    canSelectChannel: false,
+  },
+  computed: {
+  },
+  mounted() {
+    this.getAllTunnel()
+  },
+  methods: {
+
+    // 筛选 搜索
+    searchForm() {
+      // this.search()
+      this.$message.success('点击搜索按钮')
+    },
+    // 清空查询项
+    reset() {
+      this.formSearchTerm = {
+        tunnel: '-1',
+        device: '',
+        channel: '',
+      }
+      this.canSelectDevice = false
+      this.canSelectChannel = false
+    },
+    // 获取所有隧道
+    async getAllTunnel() {
+      const url = RAP2_TUNNEL_ONLINE_MONITORING + '/api/tunnel-managent/GetTunnelIdAndNameListAsync'
+      this.allTunnelArr = await fetchGet(url)
+    },
+
+
+    // 选择某隧道
+    searchSelectTunnel(tunnelId) {
+      // 后面所有下拉, 值给空
+      this.formSearchTerm.device = ''
+      this.formSearchTerm.channel = ''
+      // 后面所有下拉禁用, 防止接口响应时间过长, 优化用户体验
+      this.canSelectDevice = false
+      this.canSelectChannel = false
+      // 选中某项, 加载数据
+      if (tunnelId !== '-1') this.getAllDtsDevice(tunnelId)
+    },
+    // 获取所有设备
+    async getAllDtsDevice(tunnelId) {
+      const url = RAP2_TUNNEL_ONLINE_MONITORING + '/api/dtsequipment-managent/GetListByTunnelIdAsync'
+      const params = Qs.stringify({ tunnelId })
+      this.allDtsDeviceArr = await fetchGet(url, params)
+      // 设备下拉组件启用并置为"全部"
+      this.formSearchTerm.device = '-1'
+      this.canSelectDevice = true
+    },
+
+
+    // 选择某 dts 设备
+    searchSelectDevice(deviceId) {
+      // 后面所有下拉值给空
+      this.formSearchTerm.channel = ''
+      // 后面所有下拉禁用 防止接口响应时间过长, 优化用户体验
+      this.canSelectChannel = false
+      // 选中某项,加载数据
+      if (deviceId !== '-1') this.getAllDtsChannel(deviceId)
+    },
+    // 获取所有通道
+    async getAllDtsChannel(deviceId) {
+      const url = RAP2_TUNNEL_ONLINE_MONITORING + '/api/dtschannel-managent/GetListByDTSEquipmentIdAsync'
+      const params = Qs.stringify({ deviceId })
+      this.allDtsChannelArr = await fetchGet(url, params)
+      // 设备下拉组件启用并置为"全部"
+      this.formSearchTerm.channel = '-1'
+      this.canSelectChannel = true
+    },
+  }
+})
--- el-select 下拉选择/index.js
+++ el-select 下拉选择/index.js
@@ -1,91 +1,149 @@
-import { RAP2_TUNNEL_ONLINE_MONITORING } from '../global/globalConfig.js'
-import { fetchGet, fetchPost } from '../util/fetchRequest.js'
+import { fetchGet } from '../util/fetchRequest.js'
 
-new Vue({
+const vm = new Vue({
   el: '#app',
   data: {
-    // 搜索项
-    formSearchTerm: {
-      tunnel: '-1',
-      device: '',
-      channel: '',
-    },
+    foods: [],
+    food: '',
 
-    allTunnelArr: [],
-    allDtsDeviceArr: [],
-    allDtsChannelArr: [],
+    disabledValue: '',
 
-    canSelectDevice: false,
-    canSelectChannel: false,
+    elSelectDisable: true,
+    aArr: [],
+    a2Arr: [],
+
+    cities: [],
+    city: '',
+
+    groups: [],
+    group: '',
+
+    city2Arr: [],
+
+    emptyVal: '',
+
+    remoteVal: '',
+    remoteArr: [],
+    loading: false,
+    loading2: false,
+    list: [],
+    actualOptions: [],
+    actualOptions2: [],
   },
   computed: {
   },
   mounted() {
-    this.getAllTunnel()
+    this.getFoods()
+    this.getCities()
+    this.getGroups()
+    this.getData()
   },
   methods: {
-
-    // 筛选 搜索
-    searchForm() {
-      // this.search()
-      this.$message.success('点击搜索按钮')
+    // 获取下拉数据
+    async getFoods() {
+      this.foods = await fetchGet('mock/foods.json')
     },
-    // 清空查询项
-    reset() {
-      this.formSearchTerm = {
-        tunnel: '-1',
-        device: '',
-        channel: '',
+    async getCities() {
+      this.cities = await fetchGet('mock/cities.json')
+    },
+    async getGroups() {
+      this.groups = await fetchGet('mock/groups.json')
+    },
+
+
+    // 远程搜索, 模拟数据库所有数据
+    async getData() {
+      const res = await fetchGet('mock/states.json')
+      this.list = res.map(item => {
+        return {
+          value: `value: ${item}`,
+          label: `label: ${item}`,
+        }
+      })
+    },
+    // 远程搜索方法
+    remoteMethod(query) {
+      if (query !== '') {
+        this.loading = true;
+        // 模拟从接口获取数据这一行为
+        setTimeout(() => {
+          this.loading = false;
+          this.actualOptions = this.list.filter(item => {
+            return item.label.toLowerCase().indexOf(query.toLowerCase()) > -1;
+          });
+        }, 200);
+      } else {
+        this.actualOptions = [];
       }
-      this.canSelectDevice = false
-      this.canSelectChannel = false
     },
-    // 获取所有隧道
-    async getAllTunnel() {
-      const url = RAP2_TUNNEL_ONLINE_MONITORING + '/api/tunnel-managent/GetTunnelIdAndNameListAsync'
-      this.allTunnelArr = await fetchGet(url)
-    },
-
-
-    // 选择某隧道
-    searchSelectTunnel(tunnelId) {
-      // 后面所有下拉, 值给空
-      this.formSearchTerm.device = ''
-      this.formSearchTerm.channel = ''
-      // 后面所有下拉禁用, 防止接口响应时间过长, 优化用户体验
-      this.canSelectDevice = false
-      this.canSelectChannel = false
-      // 选中某项, 加载数据
-      if (tunnelId !== '-1') this.getAllDtsDevice(tunnelId)
-    },
-    // 获取所有设备
-    async getAllDtsDevice(tunnelId) {
-      const url = RAP2_TUNNEL_ONLINE_MONITORING + '/api/dtsequipment-managent/GetListByTunnelIdAsync'
-      const params = Qs.stringify({ tunnelId })
-      this.allDtsDeviceArr = await fetchGet(url, params)
-      // 设备下拉组件启用并置为"全部"
-      this.formSearchTerm.device = '-1'
-      this.canSelectDevice = true
+    remoteMethod2(query) {
+      if (query !== '') {
+        this.loading2 = true;
+        // 模拟从接口获取数据这一行为
+        setTimeout(() => {
+          this.loading2 = false;
+          this.actualOptions2 = this.list.filter(item => {
+            return item.label.toLowerCase().indexOf(query.toLowerCase()) > -1;
+          });
+        }, 200);
+      } else {
+        this.actualOptions2 = [];
+      }
     },
 
 
-    // 选择某 dts 设备
-    searchSelectDevice(deviceId) {
-      // 后面所有下拉值给空
-      this.formSearchTerm.channel = ''
-      // 后面所有下拉禁用 防止接口响应时间过长, 优化用户体验
-      this.canSelectChannel = false
-      // 选中某项,加载数据
-      if (deviceId !== '-1') this.getAllDtsChannel(deviceId)
+    // 获取已勾选选项
+    getCity2() {
+      console.log('this.city2Arr: ', this.city2Arr)
     },
-    // 获取所有通道
-    async getAllDtsChannel(deviceId) {
-      const url = RAP2_TUNNEL_ONLINE_MONITORING + '/api/dtschannel-managent/GetListByDTSEquipmentIdAsync'
-      const params = Qs.stringify({ deviceId })
-      this.allDtsChannelArr = await fetchGet(url, params)
-      // 设备下拉组件启用并置为"全部"
-      this.formSearchTerm.channel = '-1'
-      this.canSelectChannel = true
+
+
+    // 手动触发 focus 方法
+    emitFocus() {
+      this.$refs.oneSelect.focus()
+      console.log('emitFocus')
+      console.log('---- 分割线 ----\n\n\n')
+    },
+    // 手动触发 blur 方法
+    emitBlur() {
+      this.$refs.oneSelect.blur()
+      console.log('emitBlur')
+      console.log('---- 分割线 ----\n\n\n')
+    },
+
+    /**
+     * 以下事件方法的参都是默认参(如果有)
+     */
+    // change 事件只能手动触发, clear 事件也能触发 change 事件
+    handleSingleSelectionChange(val) {
+      console.log('handleSingleSelectionChange val: ', val)
+      console.log('---- 分割线 ----\n\n\n')
+    },
+    // clear 事件只能手动触发
+    handleSingleSelectionClear() {
+      console.log('handleSingleSelectionClear')
+      console.log('---- 分割线 ----\n\n\n')
+    },
+    // blur 事件只能手动触发, 是失去焦点
+    handleSingleSelectionBlur(e) {
+      console.log('handleSingleSelectionBlur e: ', e)
+      console.log('---- 分割线 ----\n\n\n')
+    },
+    // focus 事件只能手动触发, 是获得焦点
+    handleSingleSelectionFocus(e) {
+      console.log('handleSingleSelectionFocus e: ', e)
+      console.log('---- 分割线 ----\n\n\n')
+    },
+    // visible-change 事件只能手动触发, 下拉框出现/隐藏时触发
+    // 出现则为 true，隐藏则为 false
+    handleSingleSelectionVisibleChange(boo) {
+      console.log('handleSingleSelectionVisibleChange boo: ', boo)
+      console.log('---- 分割线 ----\n\n\n')
+    },
+    // remove-tag 事件只能手动触发, 多选模式下移除tag时触发, 移除的tag值
+    handleSingleSelectionRemoveTag(val) {
+      console.log('handleSingleSelectionRemoveTag val: ', val)
+      console.log('---- 分割线 ----\n\n\n')
     },
   }
 })
