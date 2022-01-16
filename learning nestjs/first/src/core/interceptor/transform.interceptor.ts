573009f7673c4d5d03688afb2941173f60ed104b
lpjia <420664957@qq.com>
2022-01-16 11:05:21 +0800

    20220116

--- learning nestjs/first/src/core/interceptor/transform.interceptor.ts
+++ learning nestjs/first/src/core/interceptor/transform.interceptor.ts
@@ -1,17 +1,27 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { map, Observable } from 'rxjs';
+import * as dayjs from 'dayjs'
 
 @Injectable()
 export class TransformInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle().pipe(
-      map((data) => {
-        return {
-          data,
-          code: 0,
-          msg: '请求成功'
-        }
-      })
-    );
+    return next
+      .handle()
+      .pipe(
+        map(data => {
+          // 解构变量加类型, 并重命名
+          const { createTime: cT = new Date().toString(), updateTime: uT = '222' }: { createTime: string, updateTime: string } = data
+          if (!Array.isArray(data)) {
+            if (cT) data.createTime = dayjs(cT).format('YYYY-MM-DD HH:mm')
+            if (uT) data.updateTime = dayjs(uT).format('YYYY-MM-DD HH:mm')
+          }
+
+          return {
+            data,
+            code: 0,
+            msg: '请求成功'
+          }
+        })
+      );
   }
 }
