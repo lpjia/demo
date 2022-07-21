1fff2bb651298f866f3b575495c4c653b6fdca43
lpjia <jia42066@outlook.com>
2022-07-21 13:58:58 +0800

    20220721

--- 中铭盛世/js/about.js
+++ 中铭盛世/js/about.js
@@ -1,7 +1,33 @@
 $(function () {
 
+  // 修复导航活跃样式 s
+  $(window).on('scroll', function () {
+    let pageName = $('#about_nav').data('name')
+      , idx = null
+    switch (pageName) {
+      case 'about':
+        idx = 1
+        break;
+      case 'culture':
+        idx = 2
+        break;
+      case 'school':
+        idx = 3
+        break;
+
+      default:
+        break;
+    }
+
+    $('#about_nav a').eq(idx).addClass('active')
+  });
+  // 修复导航活跃样式 e
+
+
+  // 跳转公司详情 s
   $('.company_list .sec').click(function () {
     location.href = 'subcompany.html'
   })
+  // 跳转公司详情 e
 
 })
