be9a93916aaa02e93c2ce876fe3cea577614bca8
lpjia <jia42066@outlook.com>
2022-08-08 18:19:41 +0800

    20220808

--- bianqian/index.js
+++ bianqian/index.js
@@ -4,13 +4,25 @@ function compare(a, b) {
 }
 
 window.onload = function () {
-  let dom = document.getElementById('sec')
+  const dom = document.getElementById('sec')
     , dom_2 = document.getElementById('sec_2')
     , child = dom.children // 返回 类数组 HTMLCollection
     , arr = [...child] // 想调数组的方法得先转成数组
     , arr_2 = []
+    , cacheArr = []
   for (const item of arr) {
-    arr_2.push(item.cloneNode(true)) // 实参要传 true, 否则只会克隆个 <p> 而没有内容, 因为内容也被视为节点信息
+    // arr_2.push(item.cloneNode(true)) // 实参要传 true, 否则只会克隆个 <p> 而没有内容, 因为内容也被视为节点信息
+    const node = item.cloneNode(true)
+    const word = node.innerHTML.split(' ')[0]
+
+    /* 如果数组中包含该词, 那么直接 alert 该词, 
+      不包含则继续 push, 遍历 */
+    if (cacheArr.includes(word))
+      alert(node.innerHTML)
+    else {
+      arr_2.push(node)
+      cacheArr.push(word)
+    }
   }
   arr_2.sort((a, b) => compare(a.innerHTML, b.innerHTML))
 
---
+++ 类选择器 属性选择器/index.js
@@ -0,0 +1,5 @@
+const jqdom = $('div[data-zifu|=zh]')
+  , jqdom_2 = $('[data-idx=5]')
+// jq 在这做了兼容
+console.log(jqdom)
+console.log(jqdom_2)
