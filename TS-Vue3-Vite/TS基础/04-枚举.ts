192f8dee2622182c554d715794205a149e51137c
lpjia <jia42066@outlook.com>
2023-02-13 21:35:04 +0800

    20230213

--- TS-Vue3-Vite/TS基础/04-枚举.ts
+++ TS-Vue3-Vite/TS基础/04-枚举.ts
@@ -26,4 +26,14 @@ enum StatusCode2 {
   serverError = 500,
   otherError,
 }
-console.log(StatusCode2.success, StatusCode2.serverError, StatusCode2.otherError)
+console.log('StatusCode2 枚举值: ', StatusCode2.success, StatusCode2.serverError, StatusCode2.otherError)
+console.log(StatusCode2[0], StatusCode2[1], StatusCode2[500])
+
+
+
+// 抖音上看到的, 把枚举类型变为联合类型
+type EnumToUnion = keyof (typeof StatusCode)
+const enumToUnion: EnumToUnion = 'success'
+// 不过这有什么用呢? 首先枚举自身用途是给一个映射集合, 可以双向映射
+// 其次枚举一旦声明好, 里面的key可就是只读属性了(不支持程序去修改它的值)
+// 一般是字符串枚举和数字枚举
