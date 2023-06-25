a29f49529f787317b1cbdfd055c72eeed83d6e80
lpjia <jia42066@outlook.com>
2023-06-25 23:51:33 +0800

    20230625 全国土地日

--- lodash ramda/array.js
+++ lodash ramda/array.js
@@ -1,4 +1,5 @@
 /* https://www.lodashjs.com/docs/lodash.chunk */
+/* https://ramda.cn/docs/#filter */
 
 
 const arr = [
@@ -15,18 +16,71 @@ const arr = [
 
 console.log(
   // 分成长度为3的数组串, 前端分页好使
-  _.chunk(arr, 3)
+  'R.splitEvery',
+  _.chunk(arr, 3),
+  R.splitEvery(3, arr)
 )
+console.log('---- 分割线 ----\n\n\n')
+
+
+
+// console.log(
+//   // 返回移除的项组成的数组
+//   // 会改变原数组
+//   _.remove(arr, (item) => item.score < 60),
+//   arr
+// )
+// console.log('---- 分割线 ----\n\n\n')
+
+
+
+const double = x => x * 2;
 console.log(
-  R.splitEvery(3)(arr)
+  // map遍历原始类型数组
+  'R.map',
+  R.map(double, [1, 2, 3]), //=> [2, 4, 6]
+  R.map(double, { x: 1, y: 2, z: 3 }), //=> {x: 2, y: 4, z: 6}
+  R.map(double)({ x: 1, y: 2, z: 3 }) //=> {x: 2, y: 4, z: 6}
+)
+// 60以下是D, 60-74是C, 75-89是B, 90以上是A
+const rate = (item) => {
+  if (item.score < 60) {
+    item.rate = 'D'
+  }
+  else if (item.score >= 60 && item.score < 75) {
+    item.rate = 'C'
+  }
+  else if (item.score >= 75 && item.score < 90) {
+    item.rate = 'B'
+  }
+  else if (item.score >= 90) {
+    item.rate = 'A'
+  }
+  return item
+}
+console.log(
+  R.map(rate, R.clone(arr))
+)
+console.log('---- 分割线 ----\n\n\n')
+
+
+
+
+
+const isPass = item => item.score >= 60
+console.log(
+  'R.filter',
+  R.filter(isPass, arr),
+  arr
 )
 console.log('---- 分割线 ----\n\n\n')
 
 
 
 console.log(
-  // 返回移除的项组成的数组
-  // 会改变原数组
-  _.remove(arr, (item) => item.score < 60),
+  /* 删除从start索引开始的count个元素
+  不改变原数组 */
+  'R.remove',
+  R.remove(2, 4, arr),
   arr
 )
--- new Object()/属性描述符.js
+++ new Object()/属性描述符.js
@@ -42,19 +42,85 @@ obj2.c = 'ccc'
 
 
 // 用类做一个只读属性
+// 保证程序健壮
+const aGoods = {
+  pic: './assets/g1.png',
+  title: '椰云拿铁',
+  desc: `1人份【年度重磅，一口吞云】
+
+    √原创椰云topping，绵密轻盈到飞起！
+    原创瑞幸椰云™工艺，使用椰浆代替常规奶盖
+    打造丰盈、绵密，如云朵般细腻奶沫体验
+    椰香清甜饱满，一口滑入口腔
+    
+    【饮用建议】请注意不要用吸管，不要搅拌哦~`,
+  sellNumber: 200,
+  favorRate: 95,
+  price: 32,
+}
 class UIGoods {
+  get totalPrice() {
+    return this.data.price * this.choose
+  }
+
+  get isChoose() {
+    return this.choose > 0
+  }
+
   constructor(g) {
+    g = { ...g } // 克隆, 防止原始数据被冻结, 原始数据可能其他地方也要用
+    Object.freeze(g) // 冻结克隆对象
+
     Object.defineProperty(this, 'data', {
       configurable: false,
-      get: function () {
+      get: function () { // 简写get()
         return g
       },
-      set: function (val) {
+      set: function (val) { // 简写set()
         throw new Error('data属性是只读的')
       }
     })
+
+    let internalChoose = 0
+    Object.defineProperty(this, 'choose', {
+      configurable: false,
+      get() {
+        return internalChoose
+      },
+      set(val) {
+        if (typeof val !== 'number') {
+          throw new Error('choose属性必须是数字')
+        }
+        if (val < 0) {
+          throw new Error('choose属性必须>=0')
+        }
+        if (parseInt(val, 10) !== val) {
+          throw new Error('choose属性必须是整数')
+        }
+        internalChoose = val
+      }
+    })
+
+    // // 可以换成最上面那种语法糖
+    // Object.defineProperty(this, 'totalPrice', {
+    //   get() {
+    //     return this.data.price * this.choose
+    //   }
+    // })
+
+    this.a = 1
+    // Object.freeze(this)
+    Object.seal(this) // 密封, 和freeze的区别是只要现有属性的值是可写的，它们仍然可以更改。
   }
 }
-let g = new UIGoods({})
-g.data = 1
-// 等大师课的这个知识点讲的时候再补充, 先就这
+Object.freeze(UIGoods.prototype) // 把原型也冻结了
+let g = new UIGoods(aGoods)
+// g.data = 1 // 报错
+g.choose = 0
+console.log(g.totalPrice)
+console.log(g.isChoose)
+// g.data.abc = 1 // 有可能改动原始数据, 把实参冻结
+// g.abc = 1 // 有可能往实例上加属性, 把实例冻结
+// g.a = 2 // 改动现有属性
+// UIGoods.prototype.hehe = 'haha'
+console.log(g.hehe)
