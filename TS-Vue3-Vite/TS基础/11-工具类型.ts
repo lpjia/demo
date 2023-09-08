95f822badb622e63bc417a587cf17c5b2e465b22
lpjia <jia42066@outlook.com>
2023-09-08 23:08:31 +0800

    20230908 白露 国际扫盲日

--- TS-Vue3-Vite/TS基础/11-工具类型.ts
+++ TS-Vue3-Vite/TS基础/11-工具类型.ts
@@ -1,5 +1,9 @@
 export { }
 
+/* https://www.typescriptlang.org/docs/handbook/utility-types.html */
+
+
+
 /* Partial 类型, 所有属性都设置为可选 */
 
 interface Pa {
@@ -40,7 +44,7 @@ type Tp21 = ShowMe<Required<Re>>
 
 
 
-/* Readonly 类型, 所有属性都只读 */
+/* Readonly 类型, 所有属性都只读, 不可变类型, 只能控制一层, 类似浅拷贝 */
 
 let re2: Readonly<Re> = {
   name: 're'
@@ -104,6 +108,32 @@ type RetuTp = ReturnType<(s: string) => number>
 
 
 
+/* Record 类型, 构造一个对象类型 */
+
+interface CatInfo {
+  age: number;
+  breed: string;
+}
+type CatName = "miffy" | "boris" | "mordred";
+const cats: Record<CatName, CatInfo> = {
+  miffy: { age: 10, breed: "Persian" },
+  boris: { age: 5, breed: "Maine Coon" },
+  mordred: { age: 16, breed: "British Shorthair" },
+};
+type Rec = ShowMe<Record<CatName, CatInfo>>
+// 数组
+type Arr = CatInfo[]
+type ArrTp = ShowMe<Arr>
+const arr: Arr = [
+  { age: 10, breed: "Persian" },
+  { age: 5, breed: "Maine Coon" },
+  { age: 16, breed: "British Shorthair" },
+]
+
+
+
+
+
 
 /* -------------非内置工具类型---------------------------- */
 
@@ -120,6 +150,34 @@ interface Article {
 type Optional<T, K extends keyof T> = Partial<Pick<T, K>> & Omit<T, K>
 
 type CreateArticleOptions = Optional<Article, 'author' | 'date' | 'readCount'>
+type Tp81 = ShowMe<CreateArticleOptions>
 function createArticle(options: CreateArticleOptions) {
   // options.
 }
+
+
+/* DeepReadonly 类型, 不可变类型, 深度的也可以控制 */
+
+type DeepReadonly<T extends Record<string | symbol, any>> = {
+  readonly [K in keyof T]: DeepReadonly<T[K]>
+}
+
+interface O {
+  a: number
+  b: string
+  c: boolean
+  d: {
+    dd: boolean
+  }
+}
+
+let o: DeepReadonly<O> = {
+  a: 1,
+  b: '2',
+  c: true,
+  d: {
+    dd: false
+  }
+}
+// o.a = 11 // 报错
+// o.d.dd = true // 不报错, Readonly只能控制第一层
