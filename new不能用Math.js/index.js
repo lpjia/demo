e84fe3103c5dce335f4b433c52dc5acd42db749d
lpjia <jia42066@outlook.com>
2023-10-29 23:46:36 +0800

    20231029 世界中风日

--- new不能用Math.js/index.js
+++ new不能用Math.js/index.js
@@ -1,17 +1,19 @@
 // 使用简单, 所以先不分文件写了
 
 
-// 随机数 0-1, 但取不到1
 console.log(
-  Math.random()
-)
+  /* 随机数 0-1, 但取不到1 */
+  Math.random(),
 
-// 向下取整
-console.log(
-  Math.floor(1.8)
-)
+  /* 向下取整 */
+  Math.floor(1.8),
+  Math.floor(-1.8),
 
-// 向上取整
-console.log(
-  Math.ceil(1.3)
+  /* 向上取整 */
+  Math.ceil(7.3),
+  Math.ceil(-7.3),
+
+  /* 向0取整 */
+  parseInt('5.8', 10),
+  parseInt('-5.8', 10),
 )
