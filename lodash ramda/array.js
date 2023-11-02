a8b22bd3380ffc08c561afe0b1ceab96cf29dae1
lpjia <jia42066@outlook.com>
2023-11-02 14:05:30 +0800

    20231102

--- lodash ramda/array.js
+++ lodash ramda/array.js
@@ -11,49 +11,40 @@ const arr = [
   { name: 'Zeros', score: 7 },
   { name: 'Jack', score: 67 },
 ]
-
-
-
-console.log(
-  // 分成长度为3的数组串, 前端分页好使
-  'R.splitEvery',
-  _.chunk(arr, 3),
-  R.splitEvery(3, arr)
-)
-console.log('---- 分割线 ----\n\n\n')
-
-
-
 // console.log(
-//   // 返回移除的项组成的数组
-//   // 会改变原数组
-//   _.remove(arr, (item) => item.score < 60),
-//   arr
+//   /* 数组分块 */
+//   // 分成长度为3的数组串, 前端分页好使
+//   'R.splitEvery',
+//   R.splitEvery(3, arr),
+
+//   _.chunk(arr, 3),
 // )
 // console.log('---- 分割线 ----\n\n\n')
 
 
 
-const double = x => x * 2;
-console.log(
-  // map遍历原始类型数组
-  'R.map',
-  R.map(double, [1, 2, 3]), //=> [2, 4, 6]
-  R.map(double, { x: 1, y: 2, z: 3 }), //=> {x: 2, y: 4, z: 6}
-  R.map(double)({ x: 1, y: 2, z: 3 }) //=> {x: 2, y: 4, z: 6}
-)
-// 60以下是D, 60-74是C, 75-89是B, 90以上是A
-const rate = (item) => {
+
+// const double = (x) => x * 2;
+// console.log(
+//   // map遍历原始类型数组
+//   'R.map',
+//   R.map(double, [1, 2, 3]), //=> [2, 4, 6]
+//   R.map(double, { x: 1, y: 2, z: 3 }), //=> {x: 2, y: 4, z: 6}
+//   R.map(double)({ x: 1, y: 2, z: 3 }) //=> {x: 2, y: 4, z: 6}
+// )
+
+/* 60以下是D, 60-74是C, 75-89是B, 90以上是A */
+const rate = (item = {}) => {
   if (item.score < 60) {
     item.rate = 'D'
   }
-  else if (item.score >= 60 && item.score < 75) {
+  else if (item.score < 75) {
     item.rate = 'C'
   }
-  else if (item.score >= 75 && item.score < 90) {
+  else if (item.score < 90) {
     item.rate = 'B'
   }
-  else if (item.score >= 90) {
+  else {
     item.rate = 'A'
   }
   return item
@@ -67,11 +58,17 @@ console.log('---- 分割线 ----\n\n\n')
 
 
 
-const isPass = item => item.score >= 60
+const isPass = (item) => item.score >= 60
+/* filter过滤为真值的返回
+reject过滤为假值的返回 */
 console.log(
   'R.filter',
   R.filter(isPass, arr),
   arr
+)
+console.log(
+  'R.reject',
+  R.reject(isPass, arr),
 )
 console.log('---- 分割线 ----\n\n\n')
 
@@ -83,4 +80,35 @@ console.log(
   'R.remove',
   R.remove(2, 4, arr),
   arr
+)
+
+const arrDeepClone = _.cloneDeep(arr)
+console.log(
+  /* 返回移除的项组成的数组
+  会改变原数组 */
+  '_.remove 用法和效果和R.remove都不一样',
+  _.remove(arrDeepClone, (item) => item.score < 60),
+  arrDeepClone,
+)
+console.log('---- 分割线 ----\n\n\n')
+
+
+
+
+const rate2 = (student) => {
+  const { score } = student;
+  return score < 60 ? 'D' :
+    score < 75 ? 'C' :
+      score < 90 ? 'B' :
+        'A';
+}
+/* 直接封装成纯函数, 方便调用, 也方便调试和测试 */
+const groupBy = R.groupBy(rate2)
+
+console.log(
+  'R.groupBy',
+  R.groupBy(rate2, arr),
+  groupBy(arr),
+
+  _.groupBy(arr, rate2)
 )
