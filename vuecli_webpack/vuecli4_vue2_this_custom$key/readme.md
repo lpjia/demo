a58259a45145c345e2e2768374e80899e6839fdc
lpjia <jia42066@outlook.com>
2023-12-17 05:55:30 +0800

    20231217

---
+++ vuecli_webpack/vuecli4_vue2_this_custom$key/readme.md
@@ -0,0 +1,3 @@
+vuecli4-vue2-this-custom$key
+vuecli4-vue3-this-custom$key
+这两个项目是调试this指向和vue全局自定义属性
---
+++ vuecli_webpack/vuecli4_vue3_this_custom$key/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import { fn } from "../utils/common";
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  created() {
+    fn(this)
+  },
+  mounted() {
+    console.log('组件 mounted', this) // this指向这个组件实例
+  }
+}
+</script>
