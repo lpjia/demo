cbb98a5f3a81bd58eb93624c328e30bd721fe75c
lpjia <jia42066@outlook.com>
2023-12-22 09:10:23 +0800

    20231222 冬至

--- jsDoc/jsDoc2.js
+++ jsDoc/jsDoc2.js
@@ -3,5 +3,5 @@
  * @param {import('./jsDoc.js').Gongju} personObject 想用import, 则那个文件必须有export, 否则没有提示
  */
 function greetPerson(personObject) {
-  console.log(`Hello ${personObject.lastName}`);
+  console.log(`Hello ${personObject.age}`); // 会有代码提示
 }
