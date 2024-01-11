548ecf9b53d39b1f4bd953b152ddcdf0de8ddb2b
lpjia <jia42066@outlook.com>
2024-01-12 01:45:30 +0800

    20240112

---
+++ 面试题/四角线框的跟随移动 蒙层/index.js
@@ -0,0 +1,7 @@
+const imgs = document.querySelectorAll('.container img')
+const pointer = document.querySelector('.pointer')
+for (const img of imgs) {
+  img.onmouseenter = () => {
+    pointer.style
+  }
+}
