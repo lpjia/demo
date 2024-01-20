8f00d9dc70795871c1110b0553a6ca6d6483d9ee
lpjia <jia42066@outlook.com>
2024-01-21 01:32:01 +0800

    20240121

--- 尺寸 盒子 相关计算/index.js
+++ 尺寸 盒子 相关计算/index.js
@@ -1,10 +1,12 @@
-doms = {
+const doms = {
+  devicePixelRatio: document.querySelector('#devicePixelRatio'),
   one: document.querySelector('.one'),
   one2: document.querySelector('.one2'),
   two: document.querySelector('.two'),
   two2: document.querySelector('.two2'),
   three: document.querySelector('.three'),
   three2: document.querySelector('.three2'),
+  three3: document.querySelector('.three3'),
   four: document.querySelector('.four'),
   four2: document.querySelector('.four2'),
   four3: document.querySelector('.four3'),
@@ -35,6 +37,10 @@ console.log(
   doms.three2.clientWidth,
   doms.three2.offsetWidth,
   doms.three2.scrollWidth,
+  '\nthree3:',
+  doms.three3.clientWidth,
+  doms.three3.offsetWidth,
+  doms.three3.scrollWidth,
 )
 const rect = doms.four.getBoundingClientRect()
 const rect2 = doms.four2.getBoundingClientRect()
@@ -51,4 +57,13 @@ console.log(
 
 function addGap() {
   doms.four3.style.margin = '30px'
+}
+
+doms.devicePixelRatio.textContent = `window.devicePixelRatio: ${window.devicePixelRatio}`
+
+function getDomBorder(dom, isScroll) {
+  /* 没有找到计算滚动条默认尺寸的api, 直接写死 17px
+  三元表达式的运算优先级没有 - 高
+  因为横向滚动条比较少出现, 所以用高度来计算, 宽度是一样的 */
+  return (dom.offsetHeight - dom.clientHeight - (isScroll ? 17 : 0)) / 2
 }
