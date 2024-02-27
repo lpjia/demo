727620759f9b8a0d0d1d350d8793c96b122372ef
lpjia <jia42066@outlook.com>
2024-02-27 17:36:44 +0800

    20240227

---
+++ vite-vue3/challenge_capitalize/README.md
@@ -0,0 +1,6 @@
+### vue挑战
+
+v-model.capitalize 实现大写修饰符
+
+[题目](https://cn-vuejs-challenges.netlify.app/questions/305-capitalize/readme.zh-cn.html)
+[很强的答案](https://github.com/webfansplz/vuejs-challenges/issues/308)
---
+++ vite-vue3/challenge_capitalize/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, vModelText } from 'vue'
+
+const _vModelText = vModelText
+
+_vModelText.updated = (el, { value, modifiers: { capitalize } }) => {
+  if (capitalize && value.hasOwnProperty(0)) {
+    el.value = value[0].toUpperCase() + value.substring(1)
+  }
+}
+
+const v = ref("")
+</script>
+
+<template>
+  <input type="text" v-model.capitalize="v" />
+  <br>
+  <input type="text" v-model="v" />
+</template>
