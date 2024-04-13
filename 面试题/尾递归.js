faea2bdd5a114e6c2dc9dad6c790470944c9e12a
lpjia <jia42066@outlook.com>
2024-04-13 09:23:21 +0800

    20240413

---
+++ 面试题/尾递归.js
@@ -0,0 +1,19 @@
+/* 尾递归
+wei(n-1, a*n) 调用后不再需要一步计算的过程 */
+function wei(n, a = 1) {
+  return n === 1 ? a : wei(n - 1, a * n)
+}
+console.log(
+  wei(5)
+)
+
+
+
+
+
+/* 这种写法挺别致的, 暂时不清楚有什么用 */
+async function sum(n, prevSum = 0) {
+  if (n <= 1) return n + prevSum;
+  return await Promise.resolve().then(() => sum(n - 1, n + prevSum))
+}
+sum(10).then(console.log)
