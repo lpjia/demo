32ec8037822a392f9585a77f8c09a5122f4bf82a
lpjia <jia42066@outlook.com>
2024-04-18 04:07:59 +0800

    20240418

--- 面试题/可选链操作符.js
+++ 面试题/可选链操作符.js
@@ -1,4 +1,12 @@
-// 可选链操作符
-const user = {}
+/* 可选链操作符
+当?.左边的值是 undefined 或 null 时, 不会再运算右边的, 会直接返回结果, 结果就是 undefined */
+const user = {} // 可以为任意值
 console.log('user?.address?.street: ', user?.address?.street)
-// 确定有 user 这个对象, 则可以用 user.address?.street
+
+
+if (user && user.address && user.address.street) {
+  // 以前会这样处理, 写很多重复的, 很呆
+}
+
+
+/* ES2020的新特性 */
