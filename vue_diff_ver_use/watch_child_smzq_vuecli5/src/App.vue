8df3f81125483708d108334b82cfbc2a2c6a7f49
lpjia <jia42066@outlook.com>
2024-08-08 23:46:55 +0800

    20240808 监听子组件的生命周期

---
+++ vue_diff_ver_use/watch_child_smzq_vuecli5/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" @hook:mounted="log" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    log() {
+      console.log('监听到子组件的mounted钩子函数执行')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
