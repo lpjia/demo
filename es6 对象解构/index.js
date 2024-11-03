24b22a6fd4e0bb46cecfdf1eacf6c9a2b8ed0b37
lpjia <jia42066@outlook.com>
2024-11-04 02:20:10 +0800

    20241104

--- es6 对象解构/index.js
+++ es6 对象解构/index.js
@@ -1,7 +1,8 @@
 
 /* 对象解构
-重命名用冒号:, 重命名后原变量名无效, 读原变量名会报错未定义
-解构对象没有这个属性, 那它的值就是undefined, 等于有这个属性但值为undefined
+重命名用冒号:, 冒号后面是新变量名, 重命名后原变量名无效, 读原变量名会报错未定义
+解构对象没有某个属性时, 那它的值就是undefined, 等于有这个属性但值为undefined
+只有值为undefined时, 用默认赋值才有效, null则不行
 不能解构null和undefined, 所以一般obj || {}, 提高程序健壮性
 深层解构, 用冒号和{} */
 const obj = {
@@ -12,9 +13,16 @@ const obj = {
   nullVar: null, // 只有值为undefined可以用默认赋值
   address: '美国',
 }
-const { ename = 'defaultName' /* 无效 */, money, hasMoney: isRich,
-  noExist, noExistWithDefaultValue = '默认赋值', undefinedVar = '赋值后',
-  nullVar = false, address: addr = '中国' /* 无效 */,
+const {
+  ename = 'defaultName' /* 无效 */,
+  money,
+  hasMoney: isRich,
+  noExist,
+  noExistWithDefaultValue = '默认赋值',
+  undefinedVar = '赋值后',
+  nullVar = false,
+  address: addr = '中国' /* 有原属性 address, 所以默认赋值无效 */,
+  city: town = 'zhonghe' /* 无原属性 city, 默认赋值有效, 还是符合"只有值为undefined时, 用默认赋值才有效, null则不行" */,
 } = obj || {}
 console.log('ename: ', ename)
 console.log('money: ', money)
@@ -28,11 +36,13 @@ console.log('nullVar:', nullVar)
 
 // console.log('address:', address) // 报错, 未定义
 console.log('addr:', addr)
+console.log('town:', town)
 console.log('---- 分割线 ----\n\n\n')
 
 
 
-/* 深层解构 */
+/* 深层解构
+用类似重命名的方式(导致原变量名无效) */
 const obj2 = {
   ename: 'this is obj2',
   child: {
@@ -43,8 +53,9 @@ const obj2 = {
 }
 const {
   ename: objName,
-  child, // 后续想要读child对象, 还得声明child
-  child: { childName, childAge }, // 这里冒号说明原变量已经不存在了(未定义)
+  child: { childName, childAge }, // 这里冒号说明原变量已经不存在了(未定义), 新变量用 {} 再次解构
+  child, // 后续想要读 child 属性这个对象, 还得声明child
+  parent: { parentName } = {}, // parent 属性不存在, 值为undefined, 如果没有默认赋值 = {}, 再次解构则会报错
 } = obj2 || {}
 console.log('objName: ', objName)
 console.log('childName: ', childName)
@@ -52,6 +63,7 @@ console.log('childAge: ', childAge)
 
 console.log('child: ', child)
 console.log('child.childGender:', child.childGender)
+console.log('parentName:', parentName)
 console.log('---- 分割线 ----\n\n\n')
 
 
