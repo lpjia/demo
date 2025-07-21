39e09dd6494df1fa229d872c62b5549dc5b51b3b
lpjia <jia42066@outlook.com>
2025-07-22 07:25:40 +0800

    20250722

--- 请求/$ajax.js
+++ 请求/$ajax.js
@@ -1,89 +1,39 @@
 function fn() {
   $.ajax({
-    url: 'http://localhost:7010/api/student',
-    type: 'GET',
-    async: true,
-    /* data属性赋值了obj结构, 会被默认转换成查询字符串 */
-    data: {
+    url: 'http://localhost:3000/api/login',
+    type: 'POST', // 默认值'GET'
+    // async: true, // 默认值true
+    // contentType: 'application/json;charset=utf-8', // 默认值'application/x-www-form-urlencoded'
+    data: { // data属性的值, 对象会被$ajax默认转成 查询字符串
       k1: 'v1',
       k2: 'v2',
     },
-    /* 给Ajax返回的原始数据的进行预处理 */
-    dataFilter(data, type) {
-      console.log(
-        '返回的原始数据:',
-        data,
-        typeof data, // 这个原始数据竟然是json字符串
-        type,
-      )
 
-      let obj = JSON.parse(data)
-      obj.data = '处理原始数据后新赋值'
-      console.log(
-        obj
-      )
 
-      // return data
-      return JSON.stringify(obj)
-    },
-    dataType: 'json', // 服务器返回的数据类型
-    success(res) {
-      console.log('res:', res)
-    },
-    error(xhr, textStatus, errorThrown) {
-      console.log(
-        xhr,
-        textStatus,
-        errorThrown,
-      )
-    },
-    complete(xhr, textStatus) {
-      console.log(
-        xhr,
-        textStatus,
-      )
-    }
-  })
-}
+    /* contentType: 'application/json;charset=utf-8',
+    data: JSON.stringify({ k1: 'v1', k2: 'v2', }), */
 
-function fn2() {
-  $.ajax({
-    url: 'http://localhost:7010/api/student',
-    type: 'POST',
-    async: true,
-    contentType: 'application/json;charset=utf-8',
-    /* 传json字符串, 用JSON.stringify转 */
-    data: JSON.stringify({
-      k11: 'v11',
-      k22: 'v22',
-    }),
-    /* POST请求, data属性不能直接赋值obj结构 */
-    // data: {
-    //   k11: 'v11',
-    //   k22: 'v22',
-    // },
-    success(res) {
-      console.log('res:', res)
+    /* 响应返回的原始数据 进行预处理 */
+    dataFilter(...args) {
+      console.log('dataFilter args:', args)
+      return args[0]
     },
-    error(xhr, textStatus, errorThrown) {
-      console.log(
-        xhr,
-        textStatus,
-        errorThrown,
-      )
+    dataType: 'json', // 预期服务器响应类型
+    success(...args) {
+      console.log('success args:', args)
     },
-    complete(xhr, textStatus) {
-      console.log(
-        xhr,
-        textStatus,
-      )
-    }
+    error(...args) {
+      console.log('error args:', args)
+    },
+    complete(...args) {
+      console.log('complete args:', args)
+    },
   })
 }
 
 // jQ语法
 $(function () {
 
-  fn2()
+  fn()
 
 })
