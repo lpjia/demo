712d494bd1834da9e6fcf21f8a2dea0bc5b7119b
lpjia <jia42066@outlook.com>
2025-09-10 03:57:52 +0800

    20250910

--- 模板字符串 高级用法 标签化模板/标签化模板.js
+++ 模板字符串 高级用法 标签化模板/标签化模板.js
@@ -3,25 +3,21 @@ const user = {
   age: 28,
 }
 
-function tagname() {
-  /* arguments是一个类数组
-  索引0项是一个数组, 数组由${}切割开的字符串组成, 最后可能有一个空字符串
-  其他索引项依次是${}的值 */
-  console.log(
-    arguments,
-    Array.isArray(arguments[0]),
-  )
+function tagname(...args) { // 使用剩余参数
+  console.log(args)
+  /* 第一项是个数组, 由${}切割开的字符串组成, 类似split方法, 数组项可能有空字符串
+  从第二项开始, 依次是${}的值 */
 
-  /* 返回什么, 下面的hi就接收什么 */
-  return tagname
+  return tagname // 返回自身可实现链式调用
 }
 
 /* 在模板字符串前面加一个标签名
 其实本质上是在调用函数
+
 得先定义函数, 否则报错 */
 const hi = tagname`My name is ${user.name}, I'm ${user.age} years old`
-  /* 接着书写模板字符串
-  会连着调用 */
+  /* 链式调用 */
   `123${user.age}`
+  `${user.name}456`
 
 console.log(hi)
