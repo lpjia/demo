823c916ec5fc89587aa30451876c093e16045ef6
lpjia <jia42066@outlook.com>
2025-11-23 01:32:35 +0800

    20251123

--- node_fs/ESModule模拟__dirname__filename.js
+++ node_fs/ESModule模拟__dirname__filename.js
@@ -22,4 +22,15 @@ const __dirname = path.dirname(__filename);
 // 以上3行代码可以在ESModule中得到CommonJS的全局变量 __dirname 和 __filename, 方便后续使用
 const filePath = path.resolve(__dirname, 'ocr.png')
 
-const file = fs.readFileSync(path.resolve(__dirname, 'ocr.png')) */
+const file = fs.readFileSync(path.resolve(__dirname, 'ocr.png'))
+*/
+
+
+
+/* 封装要考虑文件import.meta.url, 还有函数调用传参import.meta.url, 使用时很奇怪, 算了
+import { resolve as pathResolve, dirname as pathDirname } from 'node:path'
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = pathDirname(__filename);
+*/
