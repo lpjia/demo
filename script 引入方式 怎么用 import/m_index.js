be7d11d737c1e9e11fa4dc144d74cbb77c5218e3
lpjia <jia42066@outlook.com>
2026-03-09 00:12:42 +0800

    20260309

--- script 引入方式 怎么用 import/m_index.js
+++ script 引入方式 怎么用 import/m_index.js
@@ -1,5 +1,5 @@
 // 导入只能在模块脚本中, 模块脚本中可以写普通js
 import { formatTime } from '../util/commonMethod.js'
-console.log('这是模块脚本, 先引入, 模块脚本总是被延迟加载的')
+console.log('这是模块脚本, 先引入, 模块脚本总是被延后加载的')
 
 console.log(formatTime(new Date(), 'Y-M-D h:m'))
