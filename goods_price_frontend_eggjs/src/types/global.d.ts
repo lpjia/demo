dbec52ff90afc921552adbe23ec05a60cd2035cc
lpjia <jia42066@outlook.com>
2026-03-28 00:20:38 +0800

    20260328

--- goods_price_frontend_eggjs/src/types/global.d.ts
+++
@@ -1,5 +0,0 @@
-/* 显示详细的类型定义 */
-type ShowMe<T> = {
-  [P in keyof T]: T[P] extends object ? Expand<T[P]> : T[P];
-};
-type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
--- goods_price_frontend_eggjs/src/utils/enum.ts
+++
@@ -1,4 +0,0 @@
-export const formPlaceholder = {
-  input: '请输入',
-  select: '请选择'
-}
