9f21e2380207d028ff1fe30c278579d3b4d27357
lpjia <jia42066@outlook.com>
2026-04-07 14:51:36 +0800

    20260407

--- node_fs/path.js
+++ node_fs/path.js
@@ -2,30 +2,47 @@ const fs = require('node:fs')
 const path = require('node:path')
 
 
-/* path.resolve 解析为绝对路径 */
+console.log(__dirname)
+
+
+// /* path.resolve 解析为绝对路径
+// 从右往左拼，遇到 / 就当作根目录 */
+// console.log(
+//   path.resolve(__dirname, 'abc', '../aaa/xyz.png')
+// )
+
+
+/* 三个都返回一样的绝对路径 */
 console.log(
-  path.resolve(__dirname, 'abc', '../aaa/xyz.png')
+  // 以node启动目录为基准
+  path.resolve('.env')
+)
+console.log(
+  path.join(process.cwd(), '.env')
+)
+console.log(
+  path.resolve(process.cwd(), '.env')
 )
 
 
-/* path.join 组装路径, 不一定返回绝对路径 */
-console.log(
-  path.join('..', 'model/**/*.ts')
-)
-console.log(
-  path.join(__dirname, '..', 'model/**/*.ts')
-)
+// /* path.join 组装路径, 不一定返回绝对路径 */
+// console.log(
+//   path.join('..', 'model/**/*.ts')
+// )
+// console.log(
+//   path.join(__dirname, '..', 'model/**/*.ts')
+// )
 
 
-/* path.extname 返回扩展名(后缀名, 带.), ext是扩展名的意思 */
-console.log(
-  path.extname('index.d.ts')
-)
+// /* path.extname 返回扩展名(后缀名, 带.), ext是扩展名的意思 */
+// console.log(
+//   path.extname('index.d.ts')
+// )
 
 
 
-/* path.parse 把路径解析为一个obj */
-console.log(
-  path.parse('/a/b/c/abc.txt')
-  // { root: '/', dir: '/a/b/c', base: 'abc.txt', ext: '.txt', name: 'abc' }
-)
+// /* path.parse 把路径解析为一个obj */
+// console.log(
+//   path.parse('/a/b/c/abc.txt')
+//   // { root: '/', dir: '/a/b/c', base: 'abc.txt', ext: '.txt', name: 'abc' }
+// )
