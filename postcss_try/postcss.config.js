313e3d703650a3cfa0ae4b6bfac4382922da9b6f
lpjia <jia42066@outlook.com>
2026-05-17 02:02:54 +0800

    20260517

--- postcss_try/postcss.config.js
+++ postcss_try/postcss.config.js
@@ -4,6 +4,11 @@ export default {
     // tailwindcss: {}, // tailwindcss@3
     '@tailwindcss/postcss': {}, // tailwindcss@4
     'postcss-preset-env': {},
-    // 'postcss-modules': {}
+    /* 'postcss-modules': {} */
+
+    // 仅在环境变量 ENABLE_MODULES=true 时启用
+    ...(process.env.ENABLE_MODULES === 'true' && {
+      'postcss-modules': {}
+    })
   }
 }
