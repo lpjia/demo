ea37ee84145e22da135b089e9ce72c86cff90927
honor <jia42066a@163.com>
2026-07-08 00:03:31 +0800

    20260708

---
+++ vue3_vite/scss_module/src/App.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <div>
+    <span class="cls1">cls1</span>
+    <span class="cls2">cls2</span>
+  </div>
+
+  <div class="menu">
+    <div class="menu-item">1</div>
+    <div class="menu-item">2</div>
+    <div class="menu-item">3</div>
+    <div class="menu-item">4</div>
+    <div class="menu-item">5</div>
+    <div class="menu-item">6</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import 'styles/app.scss';
+
+.menu {
+  width: 140px;
+  height: 140px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid deepskyblue;
+  border-radius: 50%;
+
+  .menu-item {
+    position: absolute;
+  }
+}
+
+@import 'styles/math.scss';
+</style>
