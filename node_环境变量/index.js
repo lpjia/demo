6dfca1217a1899e643aebe1fac0a6d768c6b85e3
honor <jia42066a@163.com>
2026-07-09 23:58:08 +0800

    20260709

--- node_环境变量/index.js
+++
@@ -1,5 +0,0 @@
-// 如果 process.env.NODE_ENV 是 undefined，则手动赋值为 'development'
-const env = process.env.NODE_ENV || 'development';
-
-console.log(`当前环境: ${env}`);
-console.log('process.env.NODE_ENV:', process.env.NODE_ENV)
---
+++ vue3_vite/ui_secondary_packaging/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <MyInput ref="inputRef" v-model="data" placeholder="输入提示">
+      <template #prepend>
+        <el-select v-model="selectVal" placeholder="Select" style="width: 115px;">
+          <el-option label="Restart" value="1"></el-option>
+          <el-option label="Order No." value="2"></el-option>
+          <el-option label="Tel" value="3"></el-option>
+        </el-select>
+      </template>
+      <template #append>
+        <el-button :icon="Search" @click="clk"></el-button>
+      </template>
+    </MyInput>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import MyInput from './components/MyInput.vue';
+import { ElSelect, ElOption, ElButton } from 'element-plus';
+import { Search } from '@element-plus/icons-vue'
+
+const data = ref('')
+const inputRef = ref()
+const selectVal = ref()
+
+onMounted(() => {
+  data.value = '12321' // 父传子传孙 数据
+
+  console.log(inputRef.value)
+  inputRef.value.focus() // 调用el-input的focus方法
+})
+
+const clk = (e) => {
+  console.log('e:', e)
+  console.log(data.value)
+  console.log(inputRef.value)
+  console.log(selectVal.value)
+}
+</script>
