6792473deb91772c2cb775944a21bf0ac6920558
honor <jia42066a@163.com>
2026-07-26 21:49:44 +0800

    20260726

---
+++ vue_different_version_usage/batch_import_and_vuecli4/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import '@/pi_liang'
+import '@/icons'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
