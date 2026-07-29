b2767f18c577b433938ae97228f056c855d130f2
honor <jia42066a@163.com>
2026-07-30 00:13:41 +0800

    20260730

---
+++ vue_different_version_usage/watch_child_smzq_vite/README.md
@@ -0,0 +1,3 @@
+### 监听子组件的生命周期
+
+<HelloWorld @vue:mounted="log" />
---
+++ vue_different_version_usage/watch_child_smzq_vite/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+onMounted(() => {
+  console.log('child component mounted')
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
