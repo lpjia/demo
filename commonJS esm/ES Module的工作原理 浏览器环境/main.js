22373d7473f1d0f5c0330c5d98710c0f00468af8
honor <jia42066a@163.com>
2026-08-24 00:23:36 +0800

    20260824

--- commonJS esm/ES Module的工作原理 浏览器环境/main.js
+++ commonJS esm/ES Module的工作原理 浏览器环境/main.js
@@ -11,3 +11,21 @@ import('./dynamic.js').then((...args) => {
 })
 
 console.log('main.js:', foo, bar)
+
+
+/* 原生esm, 动态导入 json文件
+import一参是路径 二参需要加, { with: { type: 'json' } }
+只有 default 默认导出, 也就是data.default */
+import('./worker_type_list.json', { with: { type: 'json' } }).then(data => {
+  console.log(data.default)
+})
+/* await 写法 */
+const { default: worker_type_list } = await import('./worker_type_list.json', { with: { type: 'json' } })
+console.log(worker_type_list)
+/* 如果有构建工具, 则一般可能不需要写 with { type: 'json' } */
+
+
+
+/* 静态导入
+只有 default 默认导出
+import worker_type_list from './worker_type_list.json' with { type: 'json' } */
