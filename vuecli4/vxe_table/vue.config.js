00553e45e9782e1871c8bb16d8e31cce51d0510d
honor <jia42066a@163.com>
2026-09-02 00:01:34 +0800

    20260902

--- vuecli4/vxe_table/vue.config.js
+++ vuecli4/vxe_table/vue.config.js
@@ -2,21 +2,19 @@ module.exports = {
   productionSourceMap: false,
   chainWebpack: (config) => {
     config.optimization.splitChunks({
-      splitChunks: {
-        chunks: 'all',
-        cacheGroups: {
-          vue: {
-            name: 'chunk-vue', // 打包后的文件名
-            test: /[\\/]node_modules[\\/]vue[\\/]/,
-            priority: 15, // 优先级
-            reuseExistingChunk: true  // 如果模块已经被打包，则直接复用，避免重复
-          },
-          vendors: {
-            name: 'chunk-vendors',
-            test: /[\\/]node_modules[\\/]/,
-            priority: 10,
-            reuseExistingChunk: true
-          }
+      chunks: 'all',
+      cacheGroups: {
+        vue: {
+          name: 'chunk-vue', // 打包后的文件名
+          test: /[\\/]node_modules[\\/]vue[\\/]/,
+          priority: 15, // 优先级
+          reuseExistingChunk: true  // 如果模块已经被打包，则直接复用，避免重复
+        },
+        vendors: {
+          name: 'chunk-vendors',
+          test: /[\\/]node_modules[\\/]/,
+          priority: 10,
+          reuseExistingChunk: true
         }
       }
     })
